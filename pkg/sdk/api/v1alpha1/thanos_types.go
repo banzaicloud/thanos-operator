0519c1c8b947f7d1c545c8bac35609b575249449
tarokkk <gubasanyi@gmail.com>
2020-01-08 13:41:26 +0100

    add StoreGateway crd

--- pkg/sdk/api/v1alpha1/thanos_types.go
+++ pkg/sdk/api/v1alpha1/thanos_types.go
@@ -15,35 +15,89 @@
 package v1alpha1
 
 import (
+	"github.com/banzaicloud/logging-operator/pkg/sdk/model/secret"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // ThanosSpec defines the desired state of Thanos
 type ThanosSpec struct {
 	Remote          *Remote          `json:"remote,omitempty"`
 	ThanosDiscovery *ThanosDiscovery `json:"thanosDiscovery,omitempty"`
 	Local           *Local           `json:"local,omitempty"`
-	// StoreGateway
-	StoreGateway *StoreGateway `json:"storeGateway,omitempty"`
-	Rule         *Rule         `json:"rule,omitempty"`
+	StoreGateway    *StoreGateway    `json:"storeGateway,omitempty"`
+	Rule            *Rule            `json:"rule,omitempty"`
+	ObjectStore     string           `json:"object_store,omitempty"`
+}
+
+type TLS struct {
+	Managed     ManagedTLS    `json:"managedTLS,omitempty"`
+	Certificate secret.Secret `json:"certificate,omitempty"`
+}
+
+// TODO how the runtime generated certificate will work
+type ManagedTLS struct {
 }
 
 type Remote struct {
+	URLs []string `json:"urls"`
+	TLS  TLS      `json:"tls"`
 }
 
 type Local struct {
+	URLs []string `json:"urls"`
+	TLS  TLS      `json:"tls"`
 }
 
 type ThanosDiscovery struct {
+	LabelSelector metav1.LabelSelector `json:"labelSelector"`
+}
+
+type TimeRange struct {
+	// Start of time range limit to serve. Thanos Store will serve only metrics, which happened
+	// later than this value. Option can be a constant time in RFC3339 format or time duration
+	// relative to current time, such as -1d or 2h45m. Valid duration units are ms, s, m, h, d, w, y.
+	MinTime string `json:"minTime,omitempty"`
+	// 	End of time range limit to serve. Thanos Store
+	//	will serve only blocks, which happened eariler
+	//	than this value. Option can be a constant time
+	//	in RFC3339 format or time duration relative to
+	//	current time, such as -1d or 2h45m. Valid
+	//	duration units are ms, s, m, h, d, w, y.
+	MaxTime string `json:"maxTime,omitempty"`
 }
 
 type StoreGateway struct {
+	BaseObject `json:",inline"`
+	LogLevel   string `json:"logLevel,omitempty"`
+	LogFormat  string `json:"logFormat,omitempty"`
+	// Listen host:port for HTTP endpoints.
+	HttpAddress string `json:"httpAddress"`
+	// Time to wait after an interrupt received for HTTP Server.
+	HttpGracePeriod string `json:"http_grace_period"`
+	// Listen ip:port address for gRPC endpoints
+	GRPCAddress string `json:"grpcAddress"`
+	// Time to wait after an interrupt received for GRPC Server.
+	GRPCGracePeriod string `json:"grpcGracePeriod"`
+	// Maximum size of items held in the in-memory index cache.
+	IndexCacheSize string `json:"indexCacheSize"`
+	// Maximum size of concurrently allocatable bytes for chunks.
+	ChunkPoolSize string `json:"chunkPoolSize,omitempty"`
+	// Maximum amount of samples returned via a single Series call. 0 means no limit. NOTE: For
+	// efficiency we take 120 as the number of samples in chunk (it cannot be bigger than that), so
+	// the actual number of samples might be lower, even though the maximum could be hit.
+	StoreGRPCSeriesSampleLimit string `json:"storeGRPCSeriesSampleLimit,omitempty"`
+	// Maximum number of concurrent Series calls.
+	StoreGRPCSeriesMaxConcurrency int `json:"storeGRPCSeriesMaxConcurrency,omitempty"`
+	// Repeat interval for syncing the blocks between local and remote view.
+	SyncBlockDuration string `json:"syncBlockDuration,omitempty"`
+	// Number of goroutines to use when constructing index-cache.json blocks from object storage.
+	BlockSyncConcurrency int `json:"blockSyncConcurrency,omitempty"`
+	// TimeRanges is a list of TimeRange to partition Store Gateway
+	TimeRanges []TimeRange `json:"timeRanges,omitempty"`
 }
 
 type Rule struct {
+	BaseObject `json:",inline"`
 }
 
 // ThanosStatus defines the observed state of Thanos
--- pkg/sdk/go.mod
+++ pkg/sdk/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/go-logr/logr v0.1.0
 	github.com/onsi/ginkgo v1.8.0
 	github.com/onsi/gomega v1.5.0
+	k8s.io/api v0.0.0-20190918155943-95b840bb6a1f
 	k8s.io/apimachinery v0.0.0-20190913080033-27d36303b655
 	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
 	sigs.k8s.io/controller-runtime v0.4.0
