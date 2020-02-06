2212c0baf285f6f20d3f172a49b89fb5a3cc6624
Sándor Guba <gubasanyi@gmail.com>
2020-02-06 11:19:34 +0100

    CIFlow (#12)
    * circleci: init
    * fix lint errors
    * update controller-gen
    * go mod tidy
    * fix license
    
    Co-authored-by: Peter Szabo <asdwsda@users.noreply.github.com>

--- pkg/resources/bucketweb/poddristributionbudget.go
+++ pkg/resources/bucketweb/poddristributionbudget.go
@@ -1,10 +1,10 @@
-// Copyright © 2020 Banzai Cloud
+// Copyright 2020 Banzai Cloud
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//    http://www.apache.org/licenses/LICENSE-2.0
+//      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,6 +22,7 @@ import (
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
+//nolint:golint,unused
 func (b *BucketWeb) podDistributionBucket() (runtime.Object, reconciler.DesiredState, error) {
 	if b.ObjectStore.Spec.BucketWeb != nil {
 
--- pkg/resources/rule/rule.go
+++ pkg/resources/rule/rule.go
@@ -1,3 +1,17 @@
+// Copyright 2020 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package rule
 
 import (
@@ -81,6 +95,7 @@ func (r *Rule) resourceFactory() []resources.Resource {
 	for _, endpoint := range r.StoreEndpoints {
 		resourceList = append(resourceList, (&ruleInstance{r, endpoint.DeepCopy()}).statefulset)
 		resourceList = append(resourceList, (&ruleInstance{r, endpoint.DeepCopy()}).service)
+		resourceList = append(resourceList, (&ruleInstance{r, endpoint.DeepCopy()}).serviceMonitor)
 		resourceList = append(resourceList, (&ruleInstance{r, endpoint.DeepCopy()}).ingressHTTP)
 		resourceList = append(resourceList, (&ruleInstance{r, endpoint.DeepCopy()}).ingressGRPC)
 	}
