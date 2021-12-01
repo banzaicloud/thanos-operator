8107d5d0dea254720ca58295d8e3bd60d58132d4
Dudás Ádám <adudas@cisco.com>
2021-12-01 18:26:30 +0100

    refactor receiver statefulset resource builder method

--- pkg/resources/receiver/statefulset.go
+++ pkg/resources/receiver/statefulset.go
@@ -29,95 +29,90 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-func (r *receiverInstance) statefulset() (runtime.Object, reconciler.DesiredState, error) {
-	if r.receiverGroup != nil {
-		receiveGroup := r.receiverGroup
-
-		statefulset := &appsv1.StatefulSet{
-			ObjectMeta: receiveGroup.MetaOverrides.Merge(r.getMeta(r.receiverGroup.Name)),
-			Spec: appsv1.StatefulSetSpec{
-				Replicas: utils.IntPointer(r.receiverGroup.Replicas),
-				Selector: &metav1.LabelSelector{
-					MatchLabels: r.getLabels(),
-				},
-				ServiceName: r.getName(r.receiverGroup.Name),
-				Template: corev1.PodTemplateSpec{
-					ObjectMeta: r.getMeta(),
-					Spec: corev1.PodSpec{
-						Containers: []corev1.Container{
-							{
-								Name:  "receive",
-								Image: fmt.Sprintf("%s:%s", v1alpha1.ThanosImageRepository, v1alpha1.ThanosImageTag),
-								Args: []string{
-									"receive",
-									fmt.Sprintf("--objstore.config-file=/etc/config/%s", r.receiverGroup.Config.MountFrom.SecretKeyRef.Key),
-									fmt.Sprintf("--receive.local-endpoint=$(NAME).%s:10907", r.getName(r.receiverGroup.Name)),
-									"--receive.hashrings-file=/etc/hashring/hashring.json",
-									fmt.Sprintf("--receive.replication-factor=%d", 1),
-									"--label=receive_replica=\"$(NAME)\"",
-									"--log.level=debug",
-								},
-								Env: []corev1.EnvVar{
-									{
-										Name: "NAME",
-										ValueFrom: &corev1.EnvVarSource{
-											FieldRef: &corev1.ObjectFieldSelector{
-												FieldPath: "metadata.name",
-											},
+func (r receiverInstance) statefulset() (runtime.Object, reconciler.DesiredState, error) {
+	statefulSet := &appsv1.StatefulSet{
+		ObjectMeta: r.receiverGroup.MetaOverrides.Merge(r.getMeta(r.receiverGroup.Name)),
+		Spec: appsv1.StatefulSetSpec{
+			Replicas: utils.IntPointer(r.receiverGroup.Replicas),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: r.getLabels(),
+			},
+			ServiceName: r.getName(r.receiverGroup.Name),
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: r.getMeta(),
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "receive",
+							Image: fmt.Sprintf("%s:%s", v1alpha1.ThanosImageRepository, v1alpha1.ThanosImageTag),
+							Args: []string{
+								"receive",
+								fmt.Sprintf("--objstore.config-file=/etc/config/%s", r.receiverGroup.Config.MountFrom.SecretKeyRef.Key),
+								fmt.Sprintf("--receive.local-endpoint=$(NAME).%s:10907", r.getName(r.receiverGroup.Name)),
+								"--receive.hashrings-file=/etc/hashring/hashring.json",
+								fmt.Sprintf("--receive.replication-factor=%d", 1),
+								"--label=receive_replica=\"$(NAME)\"",
+								"--log.level=debug",
+							},
+							Env: []corev1.EnvVar{
+								{
+									Name: "NAME",
+									ValueFrom: &corev1.EnvVarSource{
+										FieldRef: &corev1.ObjectFieldSelector{
+											FieldPath: "metadata.name",
 										},
 									},
 								},
-								WorkingDir: "",
-								Ports: []corev1.ContainerPort{
-									{
-										Name:          "http",
-										ContainerPort: resources.GetPort(receiveGroup.HTTPAddress),
-										Protocol:      corev1.ProtocolTCP,
-									},
-									{
-										Name:          "grpc",
-										ContainerPort: resources.GetPort(receiveGroup.GRPCAddress),
-										Protocol:      corev1.ProtocolTCP,
-									},
-									{
-										Name:          "remote-write",
-										ContainerPort: resources.GetPort(receiveGroup.RemoteWriteAddress),
-										Protocol:      corev1.ProtocolTCP,
-									},
+							},
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "http",
+									ContainerPort: resources.GetPort(r.receiverGroup.HTTPAddress),
+									Protocol:      corev1.ProtocolTCP,
 								},
-								VolumeMounts: []corev1.VolumeMount{
-									{
-										Name:      "objectstore-secret",
-										ReadOnly:  true,
-										MountPath: "/etc/config/",
-									},
-									{
-										Name:      "hashring-config",
-										ReadOnly:  true,
-										MountPath: "/etc/hashring/",
-									},
+								{
+									Name:          "grpc",
+									ContainerPort: resources.GetPort(r.receiverGroup.GRPCAddress),
+									Protocol:      corev1.ProtocolTCP,
 								},
-								LivenessProbe:   resources.GetProbe(resources.GetPort(receiveGroup.HTTPAddress), resources.HealthCheckPath),
-								ReadinessProbe:  resources.GetProbe(resources.GetPort(receiveGroup.HTTPAddress), resources.ReadyCheckPath),
-								ImagePullPolicy: corev1.PullIfNotPresent,
+								{
+									Name:          "remote-write",
+									ContainerPort: resources.GetPort(r.receiverGroup.RemoteWriteAddress),
+									Protocol:      corev1.ProtocolTCP,
+								},
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "objectstore-secret",
+									ReadOnly:  true,
+									MountPath: "/etc/config/",
+								},
+								{
+									Name:      "hashring-config",
+									ReadOnly:  true,
+									MountPath: "/etc/hashring/",
+								},
+							},
+							LivenessProbe:   resources.GetProbe(resources.GetPort(r.receiverGroup.HTTPAddress), resources.HealthCheckPath),
+							ReadinessProbe:  resources.GetProbe(resources.GetPort(r.receiverGroup.HTTPAddress), resources.ReadyCheckPath),
+							ImagePullPolicy: corev1.PullIfNotPresent,
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "objectstore-secret",
+							VolumeSource: corev1.VolumeSource{
+								Secret: &corev1.SecretVolumeSource{
+									SecretName: r.receiverGroup.Config.MountFrom.SecretKeyRef.Name,
+								},
 							},
 						},
-						Volumes: []corev1.Volume{
-							{
-								Name: "objectstore-secret",
-								VolumeSource: corev1.VolumeSource{
-									Secret: &corev1.SecretVolumeSource{
-										SecretName: r.receiverGroup.Config.MountFrom.SecretKeyRef.Name,
-									},
-								},
-							},
-							{
-								Name: "hashring-config",
-								VolumeSource: corev1.VolumeSource{
-									ConfigMap: &corev1.ConfigMapVolumeSource{
-										LocalObjectReference: corev1.LocalObjectReference{
-											Name: r.getName("hashring-config"),
-										},
+						{
+							Name: "hashring-config",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: r.getName("hashring-config"),
 									},
 								},
 							},
@@ -125,51 +120,42 @@ func (r *receiverInstance) statefulset() (runtime.Object, reconciler.DesiredStat
 					},
 				},
 			},
-		}
+		},
+	}
 
-		statefulset.Spec.Template.Spec.Containers[0].Args = r.setArgs(statefulset.Spec.Template.Spec.Containers[0].Args)
+	statefulSet.Spec.Template.Spec.Containers[0].Args = r.setArgs(statefulSet.Spec.Template.Spec.Containers[0].Args)
 
-		if receiveGroup.DataVolume != nil {
-			if receiveGroup.DataVolume.PersistentVolumeClaim != nil {
-				statefulset.Spec.Template.Spec.Containers[0].VolumeMounts = append(statefulset.Spec.Template.Spec.Containers[0].VolumeMounts, corev1.VolumeMount{
-					Name:      "data-volume",
-					MountPath: receiveGroup.TSDBPath,
-				})
-				statefulset.Spec.VolumeClaimTemplates = []corev1.PersistentVolumeClaim{
-					{
-						ObjectMeta: r.getVolumeMeta("data-volume"),
-						Spec:       receiveGroup.DataVolume.PersistentVolumeClaim.PersistentVolumeClaimSpec,
-						Status: corev1.PersistentVolumeClaimStatus{
-							Phase: corev1.ClaimPending,
-						},
+	if r.receiverGroup.DataVolume != nil {
+		statefulSet.Spec.Template.Spec.Containers[0].VolumeMounts = append(statefulSet.Spec.Template.Spec.Containers[0].VolumeMounts, corev1.VolumeMount{
+			Name:      "data-volume",
+			MountPath: r.receiverGroup.TSDBPath,
+		})
+
+		if r.receiverGroup.DataVolume.PersistentVolumeClaim != nil {
+			statefulSet.Spec.VolumeClaimTemplates = []corev1.PersistentVolumeClaim{
+				{
+					ObjectMeta: r.getVolumeMeta("data-volume"),
+					Spec:       r.receiverGroup.DataVolume.PersistentVolumeClaim.PersistentVolumeClaimSpec,
+					Status: corev1.PersistentVolumeClaimStatus{
+						Phase: corev1.ClaimPending,
 					},
-				}
-			} else {
-				statefulset.Spec.Template.Spec.Containers[0].VolumeMounts = append(statefulset.Spec.Template.Spec.Containers[0].VolumeMounts, corev1.VolumeMount{
-					Name:      "data-volume",
-					MountPath: receiveGroup.TSDBPath,
-				})
-
-				volume, err := receiveGroup.DataVolume.GetVolume("data-volume")
-				if err != nil {
-					return statefulset, reconciler.StateAbsent, err
-				}
-
-				statefulset.Spec.Template.Spec.Volumes = append(statefulset.Spec.Template.Spec.Volumes, volume)
+				},
 			}
-		}
-
-		if r.receiverGroup.StatefulSetOverrides != nil {
-			if err := merge.Merge(statefulset, r.receiverGroup.StatefulSetOverrides); err != nil {
-				return statefulset, reconciler.StatePresent, errors.WrapIf(err, "unable to merge overrides to base object")
+		} else {
+			volume, err := r.receiverGroup.DataVolume.GetVolume("data-volume")
+			if err != nil {
+				return statefulSet, reconciler.StateAbsent, err
 			}
-		}
 
-		return statefulset, reconciler.StatePresent, nil
+			statefulSet.Spec.Template.Spec.Volumes = append(statefulSet.Spec.Template.Spec.Volumes, volume)
+		}
 	}
 
-	delete := &appsv1.StatefulSet{
-		ObjectMeta: r.getMeta(),
+	if r.receiverGroup.StatefulSetOverrides != nil {
+		if err := merge.Merge(statefulSet, r.receiverGroup.StatefulSetOverrides); err != nil {
+			return statefulSet, reconciler.StatePresent, errors.WrapIf(err, "unable to merge overrides to base object")
+		}
 	}
-	return delete, reconciler.StateAbsent, nil
+
+	return statefulSet, reconciler.StatePresent, nil
 }
