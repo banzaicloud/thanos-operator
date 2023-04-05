8476cd87b08e35710f6ea503fb610821b9229358
Alby Hernández <donfumero@gmail.com>
2023-04-05 17:24:28 +0100

    feat: Add extra args to T.Query

--- pkg/sdk/api/v1alpha1/thanos_types.go
+++ pkg/sdk/api/v1alpha1/thanos_types.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -219,6 +219,8 @@ type Query struct {
 	StoreResponseTimeout metav1.Duration `json:"storeResponseTimeout,omitempty" thanos:"--store.response-timeout=%s"`
 	// create Grafana data source
 	GrafanaDatasource bool `json:"grafanaDatasource,omitempty"`
+	// Extra arguments to be added to the container
+	ExtraArgs []string `json:"extraArgs,omitempty"`
 }
 
 type ThanosDiscovery struct {
